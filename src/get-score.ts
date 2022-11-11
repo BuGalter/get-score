a36db7136d6fbb7327bfb9017ac43a4c55c7a403
BuGalter <wardoc78@gmail.com>
2022-11-11 11:38:40 +0300

    feat: Added binary search to find score in getScore function

--- src/get-score.ts
+++ src/get-score.ts
@@ -1,6 +1,6 @@
 import { Stamp, Score, } from './types';
 
-export const getScore = (gameStamps: Stamp[], offset: number): Score => {
+/* export const getScore = (gameStamps: Stamp[], offset: number): Score => {
     if (offset <= 0) {
         return null;
     }
@@ -11,6 +11,31 @@ export const getScore = (gameStamps: Stamp[], offset: number): Score => {
         const el = gameStamps[i];
         if (el.offset === offset) {
             return el.score;
+        }
+    }
+    return null;
+};
+ */
+
+export const getScore = (gameStamps: Stamp[], offset: number): Score => {
+    if (offset <= 0) {
+        return null;
+    }
+    if (!Number.isInteger(offset)) {
+        return null;
+    }
+    let start = 0;
+    let end = gameStamps.length - 1;
+    while (start <= end) {
+        const middle = start + Math.round((end - start) / 2);
+        if (offset === gameStamps[middle].offset) {
+            return gameStamps[middle].score;
+        }
+
+        if (gameStamps[middle].offset < offset) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
         }
     }
     return null;
